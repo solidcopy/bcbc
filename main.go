63fc3645b59702f33670592d39d6bde274d04cf3
yuu <vbc@solidcopy.net>
2021-04-18 12:02:04 +0900

    コマンドライン引数から実行するサービスを決定。

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("test")
+	service := determineService()
+	fmt.Println(service)
+}
+
+// コマンドライン引数から実行するサービスを決定する。
+func determineService() (service string) {
+	flag.Parse()
+
+	service = flag.Arg(0)
+	if service == "" {
+		service = "calc"
+	}
+
+	return
 }

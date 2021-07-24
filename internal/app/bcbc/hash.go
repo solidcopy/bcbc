7e3518fb77c6768a123013baa486912ba9d9a55f
yuu <vbc@solidcopy.net>
2021-07-24 17:46:31 +0900

    不具合修正。
    MacからNAS上のハッシュファイルに書き込みを行う時、File#Sync()を実行すると
    operation not supported
    というエラーが発生したため、この処理を廃止。

--- internal/app/bcbc/hash.go
+++ internal/app/bcbc/hash.go
@@ -49,9 +49,6 @@ func hashRoutine(diskInfo *DiskInfo, progressChannel chan ProgressInfo, completi
 		}
 
 		_, err = fmt.Fprintf(hashFileOut, "%s:%x\n", fi.normPath, hash)
-		if err == nil {
-			err = hashFileOut.Sync()
-		}
 		if err != nil {
 			completionChannel <- CompletionMessage{diskInfo.id, err}
 			return
